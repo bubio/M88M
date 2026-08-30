1f143ea9ae5ed54d55d71a39c771256637511873
bubio <bubio66@gmail.com>
2026-08-30 09:41:32 +0900

    BUmpup version 1.2.1 to 1.2.2

--- src/raylib/disk_dialog.cpp
+++ src/raylib/disk_dialog.cpp
@@ -1400,7 +1400,7 @@ void UIManager::DrawSettings(PC8801::Config& cfg, PC88* pc88, CoreRunner* coreRu
     else if (activeTab == 5) { // About
         GuiLabel({ x + 20, pY, 500, 20 }, "M88M - PC-8801 Emulator for Modern Platforms");
         pY += 25;
-        GuiLabel({ x + 20, pY, 500, 20 }, "Version: 1.2.1");
+        GuiLabel({ x + 20, pY, 500, 20 }, "Version: 1.2.2");
         pY += 35;
 
         auto DrawLink = [&](float lx, float ly, const char* text, const char* url) {
